08ad45f0e51649c4f2bca3138bb78ec242fecdc9
hexhunter42 <hexhunter42@outlook.com>
2025-02-24 09:07:32 +0000

    Initialize the project

---
+++ app/src/test/java/com/job_portal/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_portal.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
